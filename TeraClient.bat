9502dc5af0f8540d80faae4e601b517ea55c09f8
Milice <robin_norling@outlook.com>
2020-05-18 23:01:48 +0200

    simplify setup

--- TeraClient.bat
+++ TeraClient.bat
@@ -2,18 +2,10 @@
 title TERA Client
 cd /d "%~dp0"
 
-node -e "" 2> NUL
-if %errorlevel% NEQ 0 (
-	echo Node.JS not found.
-	echo Please install the latest Current from https://nodejs.org/
-	pause
-	exit
-)
-
 if not exist ./config.json (
-	node --use-strict bin/configurator
+	bin\node.exe --use-strict bin/configurator
 	cls
 )
 
-node --use-strict --harmony .
+bin\node.exe --use-strict --harmony .
 pause
